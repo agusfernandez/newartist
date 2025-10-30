5f90147818318d8fefac6b3c97fa0fdad3ecf158
agusfernandez <agustina.fer91@gmail.com>
2025-10-29 22:27:12 -0300

    bad bunny header - banner

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+const toggle = document.querySelector('.toggle');
+const dropdown = document.querySelector('.dropdown-menu');
+
+if (toggle) {
+    toggle.addEventListener('click', () => {
+        dropdown.classList.toggle('show-dropdown');
+    });
+} 
